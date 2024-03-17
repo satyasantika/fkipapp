9cc5ec6f8830749659f7123150894b66d7f03796
Satya Santika <55965465+satyasantika@users.noreply.github.com>
2024-03-17 17:14:57 +0700

    edit nama pembimbing dan penguji di exam/report/date

--- app/DataTables/ViewExamRegistrationByDateDataTable.php
+++ app/DataTables/ViewExamRegistrationByDateDataTable.php
@@ -29,6 +29,21 @@ class ViewExamRegistrationByDateDataTable extends DataTable
             ->addColumn('waktu', function($row){
                 return $row->waktu_mulai.' - '.$row->waktu_akhir;
             })
+            ->editColumn('pembimbing1_nama',function($row){
+                return is_null($row->pembimbing1_nama) ? '' : $row->pembimbing1_nama ;
+            })
+            ->editColumn('pembimbing2_nama',function($row){
+                return is_null($row->pembimbing2_nama) ? '' : $row->pembimbing2_nama ;
+            })
+            ->editColumn('penguji1_nama',function($row){
+                return is_null($row->penguji1_nama) ? '' : $row->penguji1_nama ;
+            })
+            ->editColumn('penguji2_nama',function($row){
+                return is_null($row->penguji2_nama) ? '' : $row->penguji2_nama ;
+            })
+            ->editColumn('penguji3_nama',function($row){
+                return is_null($row->penguji3_nama) ? '' : $row->penguji3_nama ;
+            })
             ->setRowId('id');
     }
 
